a01988cff4b61a2305224b170e0b373febcc5831
deneb0618 <techdeneb@gmail.com>
2020-09-13 23:58:23 -0400

    initial build of gatsby demo

---
+++ gatsby-config.js
@@ -0,0 +1,73 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+// const queries = require("./src/constants/algolia")
+module.exports = {
+  siteMetadata: {
+    title: `Design Shop`,
+    description: `Gatsby Airtable Example. Built using Airtable, Algolia Search, Gatsby Background Image plugin and  React Context API. Containts two sliders, real-time Airtable updates and submenus. Styled using Styled-Components. `,
+    author: `@deneb0618`,
+    titleTemplate: `%s | Gatsby - Airtable`,
+    url: `https://gatsby-airtable-design-project.netlify.app/`,
+    image: `mainBcg.png`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`400`, `500`, `700`],
+          },
+          {
+            family: `Open Sans`,
+          },
+          {
+            family: `Caveat`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.GATSBY_AIRTABLE_API,
+        concurrency: 5,
+        tables: [
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Projects`,
+            mapping: { image: `fileNode` },
+          },
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Customers`,
+            mapping: { image: `fileNode` },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+        queries: require("./src/constants/algolia"),
+        chunkSize: 10000,
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { graphql } from "gatsby"
+import {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  GridProjects,
+} from "../components"
+import SEO from "../components/seo"
+
+const HomePage = ({ data }) => {
+  const {
+    allAirtable: { nodes: projects },
+  } = data
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Hero projects={projects} />
+      <About />
+      <GridProjects projects={projects} title="latest projects" />
+      <Survey />
+      <Slider />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      limit: 4
+      sort: { fields: data___date, order: DESC }
+    ) {
+      nodes {
+        id
+        data {
+          date
+          name
+          type
+          image {
+            localFiles {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default HomePage
